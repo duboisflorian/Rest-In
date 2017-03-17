0fecb33b7ec3db7fe7a10c4a938303f41d35eea5
duboisflorian <flo.dub305@gmail.com>
2017-03-17 12:53:12 +0100

    commit riiiennnnn

--- Rest-In_Code/src/Pandore/wwwroot/app/service/city.service.ts
+++ Rest-In_Code/src/Pandore/wwwroot/app/service/city.service.ts
@@ -7,33 +7,30 @@ export class CityService {
     constructor(
         private http: Http) { }
 
-    getAll() {
+    ip: string = "http://172.16.16.130:8000/";
+ //   ip: string = "http://127.0.0.1:8000/";
 
-     /*    return this.http.get('http://127.0.0.1:8000/pollsAPI/cities.json')
-            .map(data => data.json()); */
-       return this.http.get('http://172.16.14.52:8000/pollsAPI/cities.json')
+    getAll() {
+       return this.http.get(this.ip + 'pollsAPI/cities.json')
             .map(data => data.json());
     }
 
     getCity(i: number) {
        
-       /*  return this.http.get('http://127.0.0.1:8000/pollsAPI/cities/' + i + '.json')
-            .map(data => data.json());*/  
-         return this.http.get('http://172.16.14.52:8000/pollsAPI/cities/' + i + '.json')
+
+         return this.http.get(this.ip + 'pollsAPI/cities/' + i + '.json')
             .map(data => data.json());
     }
 
     getAllH() {
-     /*  return this.http.get('http://127.0.0.1:8000/pollsAPI/citiesh.json')
-            .map(data => data.json());*/
-        return this.http.get('http://172.16.14.52:8000/pollsAPI/citiesh.json')
+
+        return this.http.get(this.ip + 'pollsAPI/citiesh.json')
             .map(data => data.json());
     }
 
     getCityH(i: number) {
-     /*    return this.http.get('http://127.0.0.1:8000/pollsAPI/citiesh/' + i + '.json')
-            .map(data => data.json());*/
-       return this.http.get('http://172.16.14.52:8000/pollsAPI/citiesh/' + i + '.json')
+
+       return this.http.get(this.ip + 'pollsAPI/citiesh/' + i + '.json')
             .map(data => data.json());
     }
 }
